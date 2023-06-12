c532f538f539f048ff46910ad9a074e443bf6d4e
GleenPineda2000 <gleenpineda2000@gmail.com>
2023-06-11 20:43:53 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio_2/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_suma(object sender, EventArgs e)
+        {
+            var Dato1 = double.Parse(dato1.Text);
+            var Dato2 = double.Parse(dato2.Text);
+            var Resul = Dato1 + Dato2;
+
+
+           
+
+        }
+    }
+}
